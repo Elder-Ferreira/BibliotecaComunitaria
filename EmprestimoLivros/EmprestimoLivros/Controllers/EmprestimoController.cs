42c486f24ceb749eb5d6c77f2eaaaa4f76e45915
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-12-18 19:06:25 -0300

    Listando dados cadastrados

--- EmprestimoLivros/EmprestimoLivros/Controllers/EmprestimoController.cs
+++ EmprestimoLivros/EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmprestimoLivros.Controllers
 {
     public class EmprestimoController : Controller
     {
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
         }
     }
 }
