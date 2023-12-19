47aff29462b001d1c218ee40efcbefecbe5b7783
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-12-19 19:56:15 -0300

    Conexão do formulário empréstimo ao banco de dados

--- EmprestimoLivros/EmprestimoLivros/Controllers/EmprestimoController.cs
+++ EmprestimoLivros/EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -12,15 +12,32 @@ namespace EmprestimoLivros.Controllers
         {
             _db = db;
         }
+
         public IActionResult Index()
         {
             IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
             return View(emprestimos);
         }
 
+        [HttpGet]
         public IActionResult Cadastrar() 
         { 
             return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimos)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+ 
         }
     }
 }
