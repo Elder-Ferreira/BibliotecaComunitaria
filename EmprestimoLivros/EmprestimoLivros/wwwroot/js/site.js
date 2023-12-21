b6a41a2a1b666c1a8c2b2aef71c9a07df744944f
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-12-21 15:28:01 -0300

    Alterado idioma do filtro de busca para pt-BR

--- EmprestimoLivros/EmprestimoLivros/wwwroot/js/site.js
+++ EmprestimoLivros/EmprestimoLivros/wwwroot/js/site.js
@@ -1,4 +1,30 @@
 ﻿
 $(document).ready(function () {
-    $('#Emprestimos').DataTable();
+    $('#Emprestimos').DataTable({
+        language: {
+            "decimal": "",
+            "emptyTable": "No data available in table",
+            "info": "Mostrando _START_ registro de _END_ de _TOTAL_ entradas",
+            "infoEmpty": "Showing 0 to 0 of 0 entries",
+            "infoFiltered": "(filtered from _MAX_ total entries)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ entradas",
+            "loadingRecords": "Loading...",
+            "processing": "",
+            "search": "Procurar:",
+            "zeroRecords": "No matching records found",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "sortAscending": ": activate to sort column ascending",
+                "sortDescending": ": activate to sort column descending"
+            }
+        }
+        
+    });
 });
